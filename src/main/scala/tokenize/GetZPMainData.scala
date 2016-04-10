ac373db9e82b0704e2776997bb566ff719fdcc53
Kenneth Thomas <kenneththomas@Kennys-Macbook.local>
2016-04-10 19:28:32 +0530

    Json read writes added

--- src/main/scala/tokenize/GetZPMainData.scala
+++ src/main/scala/tokenize/GetZPMainData.scala
@@ -2,8 +2,9 @@ package tokenize
 
 import api.elasticsearch.ElasticsearchConfig
 import api.mongo.MongoConfig
+import com.mongodb.casbah.Imports
 import com.mongodb.casbah.Imports._
-import play.api.libs.json.{JsValue, Json, JsObject}
+import play.api.libs.json._
 
 /**
  * Created by kenneththomas on 4/10/16.
@@ -12,11 +13,27 @@ class GetZPMainData {
 
   val orderBy = MongoDBObject("_id" -> 1)
 
+  implicit val idReads: Reads[ObjectId] = new Reads[ObjectId] {
+    override def reads(json: JsValue): JsResult[Imports.ObjectId] = {
+      json.asOpt[String] map { str =>
+        if (org.bson.types.ObjectId.isValid(str))
+          JsSuccess(new ObjectId(str))
+        else
+          JsError("Invalid ObjectId %s".format(str))
+      } getOrElse (JsError("Value is not an ObjectId"))
+    }
+  }
+  implicit val idWrites = new Writes[ObjectId] {
+    def writes(oId: ObjectId): JsValue = {
+      JsString(oId.toString)
+    }
+  }
+
   def getData () = {
     val finalCount = 10//52982819
     var skip, c = 0
     val limit = 1//10000
-    var lastId = ""
+    var lastId = new ObjectId
 
     while (finalCount >= skip ) {
       val mongoClient = MongoConfig.getMongoClient("localhost", 27017)
@@ -44,15 +61,15 @@ class GetZPMainData {
     }
   }
 
-  def processBatch(data: MongoCursor) = {
-    var last_id = ""
+  def processBatch(data: MongoCursor): ObjectId = {
+    //var last_id = new ObjectId
     val tObj = new Tokenize
     var jsList: List[JsObject] = List[JsObject]()
     for(x <- data) {
       try{
         val json = Json.parse(x.toString);
         //println(json)
-        //last_id = (json \ "_id" ).as[String].trim
+        val last_id = (json \ "_id" ).as[ObjectId]
         //println("id" + last_id)
         val suppData = (json \ "value").as[JsValue]
         val tSupName = tObj.tokenizeName((suppData \ "supname").as[String])
@@ -65,12 +82,12 @@ class GetZPMainData {
         val tN2Addr = tObj.tokenizeName((suppData \ "n2addr").as[String])
         val obj = Tokenized(last_id, tSupName, tSupAddr, tConName, tConAddr, tN1Name, tN1Addr, tN2Name, tN2Addr)
         updateMongo(obj)
-
+        return last_id
       } catch {
         case e: Exception => e.printStackTrace()
       }
     }
-    last_id
+    return new ObjectId("")
   }
 
   def updateMongo(obj: Tokenized) = {
@@ -83,7 +100,9 @@ class GetZPMainData {
         "n1name" -> MongoDBList(obj.supname), "n1addr" -> MongoDBList(obj.supaddr),
         "n2name" -> MongoDBList(obj.supname), "n2addr" -> MongoDBList(obj.supaddr))
       val query = MongoDBObject("_id" -> obj.id)
-      collection.update(query, mObj)
+
+      val a = collection.update(query, mObj)
+      println(a)
     } catch {
       case e: Exception => e.printStackTrace()
     } finally {
@@ -91,7 +110,7 @@ class GetZPMainData {
     }
   }
 
-  case class Tokenized(id: String, supname: List[String], conname: List[String], n1name: List[String], n2name: List[String],
+  case class Tokenized(id: ObjectId, supname: List[String], conname: List[String], n1name: List[String], n2name: List[String],
                        supaddr: List[String], conaddr: List[String], n1addr: List[String], n2addr: List[String])
 
 
